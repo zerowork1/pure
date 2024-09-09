0aa42e3b3b21536358f0ba7661c83124a319d887
noviantikawulandari <123995313+mesumx10@users.noreply.github.com>
2024-09-09 10:21:55 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -712,4 +712,6 @@
 <a href="https://perpus.menpan.go.id/home/css/pisto/">https://perpus.menpan.go.id/home/css/pisto/</a>
 <a href="https://regionalinvestment.bkpm.go.id/psg/">https://regionalinvestment.bkpm.go.id/psg/</a>
 <a href="https://regionalinvestment.bkpm.go.id/pisto/">https://regionalinvestment.bkpm.go.id/pisto/</a>
+<a href="https://e-retribusipasar.semarangkab.go.id/css/pisang/">https://e-retribusipasar.semarangkab.go.id/css/pisang/</a>
+<a href="https://e-retribusipasar.semarangkab.go.id/css/pisangtoto/">https://e-retribusipasar.semarangkab.go.id/css/pisangtoto/</a>
 </div>
