3baeda1d9d93037bc18391776d262cb418c3c4c9
zerowork <dendisaimam5@gmail.com>
2024-09-29 20:25:00 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
+<a href="https://smanegeri7semarang.sch.id/sbpk/">https://smanegeri7semarang.sch.id/sbpk/</a>
 <a href="https://sbopoker39.com/">https://sbopoker39.com/</a>
 <a href="https://ddpi.kaltimprov.go.id/sbpkr/">https://ddpi.kaltimprov.go.id/sbpkr/</a>
 <a href="https://s1-aktuaria.fmipa.unesa.ac.id/sbopoker/">https://s1-aktuaria.fmipa.unesa.ac.id/sbopoker/</a>
