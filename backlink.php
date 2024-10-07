4985257e8bb66866ab7ef841cc1dd6eb3a231385
noviantikawulandari <123995313+mesumx10@users.noreply.github.com>
2024-10-07 21:00:18 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -766,4 +766,5 @@
 <a href="https://sidra.unesa.ac.id/data/">https://sidra.unesa.ac.id/data/</a>
 <a href="https://jsmk.ulm.ac.id/public/journals/data/">https://jsmk.ulm.ac.id/public/journals/data/</a>
 <a href="https://jsmk.ulm.ac.id/public/journals/app/">https://jsmk.ulm.ac.id/public/journals/app/</a>
+<a href="https://central-laboratory.um.ac.id/data/">https://central-laboratory.um.ac.id/data/</a>
 </div>
