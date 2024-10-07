472600b8190865fcdde9c5273780c8c6f247ae18
asg89-xx <jellybeann81@gmail.com>
2024-10-07 09:50:14 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,5 +1,5 @@
 <div style="display:none">
-<a href="https://totosuper92.com/">https://totosuper92.com/</a>
+<a href="https://totosuper81.com/">https://totosuper81.com/</a>
 <a href="https://s1-aktuaria.fmipa.unesa.ac.id/totsuper/">https://s1-aktuaria.fmipa.unesa.ac.id/totsuper/</a>
 <a href="https://s1-aktuaria.fmipa.unesa.ac.id/sbo/">https://s1-aktuaria.fmipa.unesa.ac.id/sbo/</a>
 <a href="https://smanegeri7semarang.sch.id/sbpk/">https://smanegeri7semarang.sch.id/sbpk/</a>
@@ -7,7 +7,7 @@
 <a href="https://sbopoker39.com/">https://sbopoker39.com/</a>
 <a href="https://ddpi.kaltimprov.go.id/sbpkr/">https://ddpi.kaltimprov.go.id/sbpkr/</a>
 <a href="https://sipbm.sulselprov.go.id/p/">https://sipbm.sulselprov.go.id/p/</a>
-  <a href="https://lib.radenintan.ac.id/repository/topthai/">https://lib.radenintan.ac.id/repository/topthai/</a>
+<a href="https://lib.radenintan.ac.id/repository/topthai/">https://lib.radenintan.ac.id/repository/topthai/</a>
 <a href="https://windowsmoviemakers.net/">https://windowsmoviemakers.net/</a>  
 <a href="https://dm.dlu.co.id/pis/">https://dm.dlu.co.id/pis/</a>
 <a href="https://sitrust.id/pisangbet/">https://sitrust.id/pisangbet/</a>
