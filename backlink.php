e8b47718e313f2ec15c5ce63c637f94c083c3014
zerowork <dendisaimam5@gmail.com>
2024-11-20 01:30:40 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -2,4 +2,5 @@
 <a href="https://siprima.kpu.go.id/">https://siprima.kpu.go.id/</a>
 <a href="https://boc.pom.go.id/">https://boc.pom.go.id/</a>
 <a href="https://link.kemkes.go.id/">https://link.kemkes.go.id/</a>
+<a href="https://simpel.undip.ac.id/">https://simpel.undip.ac.id/</a>
 </div>
