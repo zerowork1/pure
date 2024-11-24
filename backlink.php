9c72256d880218ef605d1194fedd278993f58383
zerowork <dendisaimam5@gmail.com>
2024-11-25 01:50:30 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -13,9 +13,6 @@
 <a href="https://siprima.kpu.go.id/">https://siprima.kpu.go.id/</a>
 <a href="https://progresi.co.id/">slot gacor</a>
 <a href="https://amdalnet-stg.menlhk.go.id/">https://amdalnet-stg.menlhk.go.id/</a>
-<a href="https://boc.pom.go.id/">situs togel online</a>
 <a href="https://link.kemkes.go.id/">https://link.kemkes.go.id/</a>
-<a href="https://simpel.undip.ac.id/">situs slot gacor</a>
-<a href="https://pamongdesa.kemendagri.go.id/">slot gacor 2024</a>
 <a href="https://kuiz.unesa.ac.id/">https://kuiz.unesa.ac.id/</a>
 </div>
