0fc5fe27a6fa4f336d8c2e367648531d569675e3
zerowork <dendisaimam5@gmail.com>
2024-11-26 13:50:02 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
+<a href="http://manfaat.pesantren-insan-pratama.sch.id/">http://manfaat.pesantren-insan-pratama.sch.id/</a>
 <a href="https://dfo.kemkes.go.id/">https://dfo.kemkes.go.id/</a>
 <a href="https://bkpsdmad.sambas.go.id/gaspol/">https://bkpsdmad.sambas.go.id/gaspol/</a>
 <a href="https://www.gazalauction.com/sewindu/">https://www.gazalauction.com/sewindu/</a>
