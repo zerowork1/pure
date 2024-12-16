ed394e4d55b97ab53c1bf845e56fbfcea11841b7
asg89-xx <jellybeann81@gmail.com>
2024-12-16 12:56:49 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -4,6 +4,7 @@
 <a href="https://sipatas.sabangkota.go.id/function/PATDA-V1/">https://sipatas.sabangkota.go.id/function/PATDA-V1/</a>
 <a href="https://apdol.sukabumikab.go.id/blog/misterhoki/">https://apdol.sukabumikab.go.id/blog/misterhoki/</a>
 <a href="https://infolaras.bpbd.garutkab.go.id/">https://infolaras.bpbd.garutkab.go.id/</a>
+<a href="https://www.enportadas.com/">https://www.enportadas.com/</a>
 <a href="https://heylink.me/totosuper-login/">totosuper</a>
 <a href="https://jurnal.polsri.ac.id/">https://jurnal.polsri.ac.id/</a>
 <a href="http://siakad.probis.um.ac.id/">http://siakad.probis.um.ac.id/</a>
