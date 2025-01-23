1dea06926ba160d26f378fb77a9d2cc1dcc5e690
zerowork <dendisaimam5@gmail.com>
2025-01-24 05:52:43 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
+<a href="https://journal-stiehidayatullah.ac.id/lengkap/">https://journal-stiehidayatullah.ac.id/lengkap/</a>
 <a href="https://tiaramas.solog.id/lengkap/">https://tiaramas.solog.id/lengkap/</a>
 <a href="https://journal.umg.ac.id/lengkap/">https://journal.umg.ac.id/lengkap/</a>
 <a href="https://journal.itera.ac.id/fly/">https://journal.itera.ac.id/fly/</a>
