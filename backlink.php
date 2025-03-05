455d7f16c0781c66bd1d6413320b2f0c27551e30
noviantikawulandari <123995313+mesumx10@users.noreply.github.com>
2025-03-05 16:16:43 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
+<a href="https://www.rpg.co.id/assets/sh/">https://www.rpg.co.id/assets/sh/</a>
 <a href="https://perpustakaan.poltekkesjogja.ac.id/perpustakaannthai/">https://perpustakaan.poltekkesjogja.ac.id/perpustakaannthai/</a>
 <a href="https://akuntansi.itny.ac.id/akuntansithai/">https://akuntansi.itny.ac.id/akuntansithai/</a>
 <a href="https://oceandata.hangtuah.ac.id/oceanthai/">https://oceandata.hangtuah.ac.id/oceanthai/</a>
